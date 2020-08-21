2c72565daa95d121b84de430a1e6f0f01ce4bd10
codebreak0 <salinakarki00@gmail.com>
2020-08-21 20:52:05 +0545

    edit model in views

--- datahandle/views.py
+++ datahandle/views.py
@@ -13,21 +13,6 @@ from django.contrib import messages
 User = get_user_model()
 
 
-
-def TotalPrice(request,user_id):
-    user=User.objects.get(id=user_id)
-    cartuser=user.cart_set.all()
-    orderlist=[]
-    for i in cartuser:
-        orderlist.append[i.order_set.all()]
-    countoforder=len(orderlist)
-    loopcontroller=0
-    sum=0
-    while loopcontroller < len(countoforder):
-        sum=sum+int(countoforder[loopcontroller].prod.price)
-        s=s+1
-    return render(request,'',{'sum':sum})
-
 @api_view(['GET'])
 def info_view_cat(request, pk):
     if request.method == 'GET':
