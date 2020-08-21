62b90d4f522405913efc8eac38c3396e41ef21ef
codebreak0 <salinakarki00@gmail.com>
2020-08-21 20:44:10 +0545

    add and remove card

--- datahandle/serializers.py
+++ datahandle/serializers.py
@@ -1,16 +1,32 @@
 from rest_framework import serializers
-from .models import Category,Product
+from .models import Category,Product, Cart, Order
 
 
 class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id','name','desc','image','price','in_stock']
+        fields = ['id', 'name', 'desc', 'image', 'price', 'in_stock', 'category']
+
 
 class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
     # products = serializers.StringRelatedField(many=True,read_only=True)
+
     class Meta:
         model = Category
-        fields = ('name','products')
+        fields = ('name', 'products')
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cart
+        fields = ['username', 'item', 'quantity', 'created_date']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ['username', 'item', 'quantity', 'date', 'order_in_cart', 'ordered']
--- datahandle/views.py
+++ datahandle/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
-from .models import Product,Category
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CategorySerializer,ProductSerializer
 from .models import Product,Order,Cart,Category
 from django.contrib.auth import get_user_model
-User=get_user_model()
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.contrib import messages
+User = get_user_model()
+
+
 @api_view(['GET'])
-def info_view_cat(request,pk):
-    if request.method=='GET':
+def info_view_cat(request, pk):
+    if request.method == 'GET':
         try:
-            obj=Category.objects.get(id=pk)
+            obj = Category.objects.get(id=pk)
             catprod = Product.objects.filter(prod=obj)
             b=[]
             for i in catprod:
@@ -50,12 +56,14 @@ def info_view_cat(request,pk):
 
         return Response(serializer.data)
 
+
 @api_view(['GET'])
 def info_view(request):
     if request.method=='GET':
         queryset=Category.objects.all()
         serializer=CategorySerializer(instance=queryset,many=True)
         return Response(serializer.data)
+
 
 @api_view(['GET'])
 def info_product_view(request):
@@ -68,6 +76,7 @@ def info_product_view(request):
         serializer=ProductSerializer(instance=queryset,many=True)
 
         return Response(serializer.data)
+
 
 @api_view(['GET'])
 def info_view_prod(request,pk):
@@ -117,6 +126,7 @@ def info_view_prodindividual(request,pk):
 
         return Response(serializer.data)
 
+
 def TotalPrice(request,user_id):
     user=User.objects.get(id=user_id)
     cartuser=user.cart_set.all()
@@ -132,3 +142,63 @@ def TotalPrice(request,user_id):
     return render(request,'',{'sum':sum})
 
 
+def add_to_cart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    order_item, created = Cart.objects.get_or_create(
+        item=item,
+        user=request.user
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.orderitems.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect("template:home")    # put the location of home.html and it
+                                                # redirects to where the home page is
+        else:
+            order.orderitems.add(order_item)
+            messages.info(request, "This item was added to your cart.")
+            return redirect("template:home")
+    else:
+        order = Order.objects.create(
+            user=request.user)
+        order.orderitems.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+        return redirect("template:home")
+
+
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    cart_qs = Cart.objects.filter(user=request.user, item=item)
+    if cart_qs.exists():
+        cart = cart_qs[0]
+        # Checking the cart quantity
+        if cart.quantity > 1:
+            cart.quantity -= 1
+            cart.save()
+        else:
+            cart_qs.delete()
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.orderitems.filter(item__slug=item.slug).exists():
+            order_item = Cart.objects.filter(
+                item=item,
+                user=request.user,
+            )[0]
+            order.orderitems.remove(order_item)
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("template:home")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("template:home")
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:home")
